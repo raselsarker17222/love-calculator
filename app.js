81eba39a1bffd63804a963c3dbd1433d3f6a324d
Rasel Sarker <thirstforprogramming@gmail.com>
2021-05-16 02:10:21 +0600

    love calculator with vanilla javascript

---
+++ app.js
@@ -0,0 +1,5 @@
+let yourName = prompt("What is your name?");
+let loverName = prompt("What is your lover's name?");
+let num = Math.random() * 100;
+let result = Math.floor(num) + 1;
+document.write("The love percentage between " + yourName + " and " + loverName + " is : " + result + "% .");
